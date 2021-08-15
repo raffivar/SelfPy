3e7586d13dab750e02f920486c4d8761e405dc6d
cookienator <rafivar@gmail.com>
2021-08-15 16:02:29 +0300

    improved my_mp3_playlist exercise

--- unit_9_files/my_mp3_playlist/my_mp3_playlist.py
+++ unit_9_files/my_mp3_playlist/my_mp3_playlist.py
@@ -5,8 +5,8 @@ def song_length(formatted_song):
 def my_mp3_playlist(file_path):
     with open(file_path, "r") as file1:
         song_list = file1.read().split('\n')
-        formatted_list = []
 
+        formatted_list = []
         for song in song_list:
             formatted_song = song.split(';')
             formatted_list.append(formatted_song)
@@ -17,11 +17,9 @@ def my_mp3_playlist(file_path):
         artist_mentioned_most = ("", 0)
         for formatted_song in formatted_list:
             artist_name = formatted_song[1]
-            times_mentioned_so_far = 0
-            if artist_name in artist_dict:
-                times_mentioned_so_far = artist_dict[artist_name]
-            artist_dict[artist_name] = times_mentioned_so_far + 1
-            if artist_dict[artist_name] > artist_mentioned_most[1]:
+            times_mentioned_so_far = artist_dict[artist_name] + 1 if artist_name in artist_dict else 1
+            artist_dict[artist_name] = times_mentioned_so_far
+            if times_mentioned_so_far > artist_mentioned_most[1]:
                 artist_mentioned_most = artist_name, artist_dict[artist_name]
 
         return longest_song[0], len(formatted_list), artist_mentioned_most[0]
