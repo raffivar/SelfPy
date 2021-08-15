5b6db7dba87ca5ffd08c3f270993270690ea2a90
cookienator <rafivar@gmail.com>
2021-08-15 15:57:40 +0300

    improved my_mp3_playlist exercise

--- unit_9_files/my_mp3_playlist/my_mp3_playlist.py
+++ unit_9_files/my_mp3_playlist/my_mp3_playlist.py
@@ -2,30 +2,27 @@ def song_length(formatted_song):
     return float(formatted_song[2].replace(':', '.'))
 
 
-def artist(formatted_song):
-    return formatted_song[1]
-
-
 def my_mp3_playlist(file_path):
     with open(file_path, "r") as file1:
         song_list = file1.read().split('\n')
         formatted_list = []
+
         for song in song_list:
             formatted_song = song.split(';')
             formatted_list.append(formatted_song)
+
         longest_song = max(formatted_list, key=song_length)
-        # performer_max_times = max(formatted_list, key=artist)
+
         artist_dict = {}
+        artist_mentioned_most = ("", 0)
         for formatted_song in formatted_list:
             artist_name = formatted_song[1]
+            times_mentioned_so_far = 0
             if artist_name in artist_dict:
-                artist_dict[artist_name] += 1
-            else:
-                artist_dict[artist_name] = 1
-        artist_mentioned_most = ("", 0)
-        for key, value in artist_dict.items():
-            if value > artist_mentioned_most[1]:
-                artist_mentioned_most = key, value
+                times_mentioned_so_far = artist_dict[artist_name]
+            artist_dict[artist_name] = times_mentioned_so_far + 1
+            if artist_dict[artist_name] > artist_mentioned_most[1]:
+                artist_mentioned_most = artist_name, artist_dict[artist_name]
 
         return longest_song[0], len(formatted_list), artist_mentioned_most[0]
 
